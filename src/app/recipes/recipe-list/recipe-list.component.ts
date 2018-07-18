0a3a7dea64169f089660fee0fc8436eb7810b177
jromano8954 <jromano6556@gmail.com>
2018-07-17 23:51:00 -0400

    Setting up RecipeList

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Recipe} from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  imgPath: string = 'https://www.seriouseats.com/recipes/images/2016/12/20161201-crispy-roast-potatoes-29.jpg';
+  recipes: Recipe[] = [
+    new Recipe('A test recipe','This is a test',this.imgPath),
+    new Recipe('A test recipe','This is a test',this.imgPath)
+  ];
 
   constructor() { }
 
