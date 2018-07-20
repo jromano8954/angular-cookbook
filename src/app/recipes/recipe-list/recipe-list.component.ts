f16834066c3027b8b7b18043e145f52542d12af8
jromano8954 <jromano6556@gmail.com>
2018-07-20 09:50:42 -0400

    Set up recipeItem and recipeList functionality

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,6 +11,8 @@ export class RecipeListComponent implements OnInit {
   imgPath: string = 'https://www.seriouseats.com/recipes/images/2016/12/20161201-crispy-roast-potatoes-29.jpg';
   recipes: Recipe[] = [
     new Recipe('A test recipe','This is a test',this.imgPath),
+    new Recipe('A test recipe','This is a test',this.imgPath),
+    new Recipe('A test recipe','This is a test',this.imgPath),
     new Recipe('A test recipe','This is a test',this.imgPath)
   ];
 
