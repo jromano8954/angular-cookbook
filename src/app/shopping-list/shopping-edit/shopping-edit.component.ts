ffe051e4df74efc45544999a82c7592e10795e2e
jromano8954 <jromano6556@gmail.com>
2018-07-20 19:43:27 -0400

    Set up ShoppingList Add functionality

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') itemName:ElementRef;
+  @ViewChild('amountInput') itemAmount:ElementRef;
+  @Output() addShoppingListItem = new EventEmitter<Ingredient>();
+  newItem: Ingredient;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddItem(){
+    this.newItem = new Ingredient(this.itemName.nativeElement.value,
+      this.itemAmount.nativeElement.value); 
+    this.addShoppingListItem.emit(this.newItem);
+    //console.log(this.newItem.name);
   }
 
 }
